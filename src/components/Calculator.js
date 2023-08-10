fe89dd4148c45dc948767c3839d8b0753275f859
mershark <mersharktakyi@gmail.com>
2023-08-10 10:54:57 +0000

    Create app components

---
+++ src/components/Calculator.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import './Calculator.css';
+import Button from './Buttons';
+
+const Calculator = () => (
+  <div className="calculator">
+    <div className="calculator-display">0</div>
+    <div className="calculator-grid">
+      <Button>AC</Button>
+      <Button>+/-</Button>
+      <Button>%</Button>
+      <Button>÷</Button>
+      <Button>7</Button>
+      <Button>8</Button>
+      <Button>9</Button>
+      <Button>x</Button>
+      <Button>4</Button>
+      <Button>5</Button>
+      <Button>6</Button>
+      <Button>-</Button>
+      <Button>1</Button>
+      <Button>2</Button>
+      <Button>3</Button>
+      <Button>+</Button>
+      <Button width="140%">0</Button>
+      <Button>.</Button>
+      <Button>=</Button>
+    </div>
+  </div>
+);
+
+export default Calculator;
